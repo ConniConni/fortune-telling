ab6155642c846814f28dbb3de5821b685e675395
ConniConni <nmtgktmo@gmail.com>
2025-07-07 17:38:35 +0900

    run_app()からユーザー名取得機能を別の関数に分離

--- uranai/main.py
+++ uranai/main.py
@@ -3,24 +3,24 @@ import datetime
 from .core import get_fortune_telling
 
 
-def run_app():
-    """
-    アプリケーションのメイン関数。処理は以下２つ。
-    1. ユーザー名を尋ねる
-    2. ユーザーに占い結果を表示する
-    """
+def get_user_name():
+    """有効なユーザー名を取得する関数"""
 
     while True:
         print("お名前を教えてください")
         input_user_name = input(">>> ")
         user_name = input_user_name.strip()
 
-        if not user_name:
+        if user_name:
+            return user_name
+
+        else:
             print("名前は1文字以上で入力してください")
-            continue
 
-        break
 
+def run_app():
+    """アプリケーションのメイン関数"""
+    user_name = get_user_name()
     today = datetime.date.today()
     print(f"{user_name}さんの{today.year}年{today.month}月{today.day}日の運勢は…")
     get_fortune_telling()
