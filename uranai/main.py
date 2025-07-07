180f4c9862f58a45f83775dfa1cf6950b2de6509
ConniConni <nmtgktmo@gmail.com>
2025-07-07 14:44:35 +0900

    アプリ起動時の年月日を表示する

--- uranai/main.py
+++ uranai/main.py
@@ -1,2 +1,7 @@
+import datetime
+
+
 def run_app():
-    print("プレースホルダー関数")
+    """アプリケーションのメイン関数"""
+    today = datetime.date.today()
+    print(f"あなたの{today.year}年{today.month}月{today.day}日の運勢は…")
