a659a81d92cddf993fd65b56e7384f3083088ef5
ConniConni <nmtgktmo@gmail.com>
2025-07-07 18:35:54 +0900

    占い結果のの取得元となるリストを作成

--- uranai/core.py
+++ uranai/core.py
@@ -1,2 +1,14 @@
+import random
+
+
+FORTUNES = ["大吉", "中吉", "吉", "小吉", "末吉", "凶"]
+LUCKY_COLORS = ["赤", "青", "緑", "紫", "黄", "橙", "白", "黒"]
+ADVICES = [
+    "挨拶をしよう!",
+    "感謝の気持ちを伝えよう！",
+    "慎重になろう！",
+    "チャレンジしよう！",
+]
+
+
 def get_fortune_telling():
-    print("占い結果xxxx")
