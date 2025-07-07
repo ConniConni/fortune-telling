463951b7d5ce8788592ddc46b60d5b6979dfc5a7
ConniConni <nmtgktmo@gmail.com>
2025-07-07 17:01:42 +0900

    ユーザーの名前を尋ね、今日の運勢を表示する

--- uranai/main.py
+++ uranai/main.py
@@ -2,6 +2,22 @@ import datetime
 
 
 def run_app():
-    """アプリケーションのメイン関数"""
+    """
+    アプリケーションのメイン関数。処理は以下２つ。
+    1. ユーザー名を尋ねる
+    2. ユーザーに占い結果を表示する
+    """
+
+    while True:
+        print("お名前を教えてください")
+        input_user_name = input(">>> ")
+        user_name = input_user_name.strip()
+
+        if not user_name:
+            print("名前は1文字以上で入力してください")
+            continue
+
+        break
+
     today = datetime.date.today()
-    print(f"あなたの{today.year}年{today.month}月{today.day}日の運勢は…")
+    print(f"{user_name}さんの{today.year}年{today.month}月{today.day}日の運勢は…")
