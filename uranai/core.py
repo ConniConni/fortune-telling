cd5edc1215d5792dcb191d857a04c69bccf59880
ConniConni <nmtgktmo@gmail.com>
2025-07-07 18:37:01 +0900

    占い結果をリストから取得し辞書型にする

--- uranai/core.py
+++ uranai/core.py
@@ -9,6 +9,25 @@ ADVICES = [
     "慎重になろう！",
     "チャレンジしよう！",
 ]
+RESULT_KEY = ["今日の運勢", "ラッキーカラー", "ラッキーナンバー", "一言アドバイス"]
 
 
 def get_fortune_telling():
+    """
+    占い結果を取得する関数
+    リストからランダムに取得する
+    結果は辞書型で返却する
+    """
+
+    result = {}
+
+    choice_fortune = random.choice(FORTUNES)
+    result[RESULT_KEY[0]] = choice_fortune
+    choice_lucky_number = random.randint(1, 9)
+    result[RESULT_KEY[1]] = choice_lucky_number
+    choice_lucky_color = random.choice(LUCKY_COLORS)
+    result[RESULT_KEY[2]] = choice_lucky_color
+    choice_advice = random.choice(ADVICES)
+    result[RESULT_KEY[3]] = choice_advice
+
+    return result
